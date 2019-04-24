16b4f0efce949a9d6e58c0cf6ce0d2e1c903b037
byemane <beteyemane@hotmail.co.uk>
2019-04-24 18:34:07 +0100

    search bar working

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,30 +5,51 @@ class Home extends React.Component{
   constructor() {
     super()
     this.state = {
+      search: '',
       movies: []
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.getMovies = this.getMovies.bind(this)
   }
 
-  componentDidMount(){
+  getMovies() {
     axios.get('https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup', {
       params: {
-        term: 'legallyblonde',
+        term: this.state.search,
         country: 'uk'
       },
       headers: {
-        'X-RapidAPI-Host': `${process.env.UTELLY_HOST}`,
-        'X-RapidAPI-Key': `${process.env.UTELLY_KEY}`
+        'X-RapidAPI-Key': `${process.env.UTELLY_KEY}`,
+        'X-RapidAPI-Host': `${process.env.UTELLY_HOST}`
       }
     })
       .then(res => {
-        this.setState({movies: res.data})
+        this.setState({movies: res.data.results})
       })
+      .catch(err => console.log(err))
   }
 
+  handleChange(e) {
+    e.preventDefault()
+    this.setState({search: e.target.value})
+    this.getMovies()
+  }
+
+
   render(){
-    console.log(this.state.movies)
+    console.log('movies', this.state.movies)
+    console.log('search', this.state.search)
     return(
-      <h1>Playerwatch</h1>
+      <section>
+        <h1>Playerwatch</h1>
+        <input
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={this.state.search}
+          onChange={this.handleChange}/>
+        {this.state.movies.map(movie => <div key={movie.id}> <h3>{movie.name}</h3> </div>)}
+      </section>
     )
   }
 }
